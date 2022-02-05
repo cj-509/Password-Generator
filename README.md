5a39d62ec6f4a2c13d2ba7f8ade24fcfc445d265
cj-509 <99097299+cj-509@users.noreply.github.com>
2022-02-05 14:51:42 -0700

    modified the README file

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Password-Generator
  My first ever programing project
+
+"The secrets module is used for generating cryptographically strong random numbers 
+suitable for managing data such as passwords, account authentication, 
+security tokens, and related secrets" -- python documents
---
+++ main.py
@@ -0,0 +1,23 @@
+import secrets
+import string
+
+
+upper = string.ascii_uppercase
+lower = string.ascii_lowercase
+digits = string.digits
+symbols = string.punctuation
+
+#add together all the the uppercase, lowercase, numbers(digits), and the symbols(*/-&^%$) together to the 'res' var
+res = upper + lower + digits + symbols
+char = int(input("Enter the number of characters for your password: ")) #getting the charcther lenght from the user
+while True:
+    if char < 8: # character leght cannot be < (less than) 8 
+        print("Sorry, the lenght of your password cannot be less than 8 character :(")
+        break
+    else:
+        #joinning together the character that's equals to the number of the lenght of the password
+        result = ''.join(secrets.choice(res) for i in range(char))
+        print("=" *30 + '\n')
+        print(f'Your password is: {result} \n')
+        print("=" *30 + '\n')
+        break
